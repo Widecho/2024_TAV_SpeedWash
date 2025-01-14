ac440b0c832fa6084f82eee43705a593be722214
Kornelia <tusa.kornelia@gmail.com>
2025-01-14 21:25:37 +0100

    Kezdőlap nézet2

---
+++ Documents/Prototype/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kezdőlap</title>
+    <link rel="stylesheet" >
+</head>
+<body>
+    <header>
+        <h1>Üdvözlünk a SpeedWash rendszerben!</h1>
+        <?php  ?>
+    </header>
+    <main>
+        <section> 
+            <h2>Üdvözlünk a SpeedWash mosodánál!</h2> 
+            <p>Kényelmesen kezelheted mosási folyamataidat online. Foglalj mosógépet, nézd meg a gépek állapotát!</p> 
+        </section>
+    </main>
+</body>
+</html>
