ec4676a6e1220bfb6b44503a40cecd928b4b485a
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 10:46:40 +0100

    Sikeres regisztráció nézet befejezve

--- Application/views/success.php
+++ Application/views/success.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sikeres Regisztráció</title>
-    <link rel="stylesheet">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
-
 <body>
     <header>
         <h1>Regisztráció sikeres!</h1>
-        <?php ?>
+        <?php include '../api/nav.php'; ?>
     </header>
-
     <main>
         <p>Gratulálunk! Sikeresen regisztráltál a SpeedWash rendszerébe!</p>
+        <p>Most már <a href="login.php">bejelentkezhetsz</a>, vagy visszatérhetsz a főoldalra.</p>
+        <button onclick="window.location.href='index.php';">Vissza a főoldalra</button>
     </main>
 </body>
 </html>
