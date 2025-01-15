99165c3b240f89225b11a411893de66379088241
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 12:29:49 +0100

    Mosógépek nézet elkészült

--- Application/views/machines.php
+++ Application/views/machines.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mosógépek</title>
+    <link rel="stylesheet" href="../css/styles.css"> 
+    <script src="../js/main.js" defer></script>
 </head>
 <body>
-
+    <header>
+        <h1>Foglalható mosógépeink</h1>
+        <?php include '../api/nav.php'; ?>
+    </header>
+    <main>
+        <div id="machines-container"> </div>
+    </main>
 </body>
 </html>
