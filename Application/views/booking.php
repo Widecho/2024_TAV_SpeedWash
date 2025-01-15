bc5c54ee5d767d2899f937205ce8a9e28a6c6f49
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 12:08:41 +0100

    Foglalás nézet elkezdve

---
+++ Application/views/booking.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foglalás</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/main.js" defer></script>
+</head>
+<body>
+    <header>
+        <h1>Foglalás</h1>
+        <?php include '../api/nav.php'; ?>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
