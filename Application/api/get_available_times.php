6a134c39d685bdf0af4558699978fdb61b463d94
Csatlós Krisztián <christian.csatlos@gmail.com>
2025-01-15 09:58:14 +0100

    Dátum- és időkezelés bevezetése

--- Application/api/get_available_times.php
+++ Application/api/get_available_times.php
@@ -9,5 +9,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo json_encode(['success' => false, 'message' => 'Mosógép azonosító szükséges.']);
         exit;
     }
+    $current_time = strtotime('now');
+    $next_quarter = ceil($current_time / (15 * 60)) * (15 * 60);
+    $start_of_day = max($next_quarter, strtotime("$date 00:00:00"));
+    $end_of_day = strtotime("$date 23:59:59");
+    $duration_seconds = 3600; // Egyórás időtartam
 }
 ?>
