08d5e6d15d39d971b0e589c73fe3f599831017f7
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 01:38:58 +0100

    Bejelentkezés nézet elkezdev, felesleges fájl törölve

---
+++ Application/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bejelentkezés</title>
+    <link rel="stylesheet">
+    <script></script>
+</head>
+<body>
+    <header>
+        <h1>Felhasználói Bejelentkezés</h1>
+        <?php ?>
+    </header>
+    <main>
+        
+    </main>
+    
+</body>
+</html>
