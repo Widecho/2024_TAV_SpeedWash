0c8688231c7f71eeae16708dedbb134aff70c936
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 10:33:12 +0100

    Bejelentkezés nézet befejezve

--- Application/views/login.php
+++ Application/views/login.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bejelentkezés</title>
-    <link rel="stylesheet">
-    <script></script>
+    <link rel="stylesheet" href="../css/styles.css">
+    <script src="../js/main.js" defer></script>
 </head>
 <body>
     <header>
         <h1>Felhasználói Bejelentkezés</h1>
-        <?php ?>
+        <?php include '../api/nav.php'; ?>
     </header>
     <main>
-        
+        <form id="login-form">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="password">Jelszó:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Bejelentkezés</button>
+        </form>
+        <p id="response-message"></p>
     </main>
     
 </body>
