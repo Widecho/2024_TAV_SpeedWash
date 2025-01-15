a79d3c74ad4359449b553deba29f39b34b4ce494
Kornelia <tusa.kornelia@gmail.com>
2025-01-15 09:43:06 +0100

    Sikeres nézet elkezdve

---
+++ Application/views/success.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sikeres Regisztráció</title>
+    <link rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1>Regisztráció sikeres!</h1>
+        <?php ?>
+    </header>
+
+    <main>
+        <p>Gratulálunk! Sikeresen regisztráltál a SpeedWash rendszerébe!</p>
+    </main>
+</body>
+</html>
