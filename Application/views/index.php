8a8a0ebe44d72650d06de3832867afcc646c9774
Kornelia <tusa.kornelia@gmail.com>
2025-01-16 22:05:01 +0100

    Index nézet kiegészítve

--- Application/views/index.php
+++ Application/views/index.php
@@ -16,6 +16,60 @@
             <h2>Üdvözlünk a SpeedWash mosodánál!</h2> 
             <p>Kényelmesen kezelheted mosási folyamataidat online. Foglalj mosógépet, nézd meg a gépek állapotát!</p> 
         </section>
+        <img src="images/logo.png" alt="logo" height="150">
+           
+            <h3>"SpeedWash ahol a tisztaság minőséggel és megbízhatósággal párosul!"</h3>
+    </main>
+    <h5>Legyen szó mindennapi mosásról vagy kényes ruhadarabokról, a SpeedWash mosoda garantálja a tökéletes eredményt. Korszerű berendezéseink és gondosan válogatott tisztítószereink biztosítják, hogy ruháid nemcsak tiszták, hanem meg is őrzik eredeti állapotukat.</h5>
+            Miért bízhatsz bennünk?
+        
+<p>🔹 Prémium minőség minden mosásnál</p>
+<p>🔹 Korszerű, ruhabarát technológia</p>
+<p>🔹 Megbízható szolgáltatás amit ígérünk, azt teljesítjük</p>
+<p>🔹 Hosszú távú elégedettség</p>
+
+        <p>A SpeedWash-nál nemcsak a <b>gyosaság</b>ot, hanem a <b>minőség</b>et és a <b>megbízhatóság</b>ot is garantáljuk. Próbáld ki most, és tapasztald meg, milyen az igazán profi mosoda!</p>
+       <b><u>Gépeink:</u></b>
+       
+       <table>
+        <tr>
+            <td rowspan="9"><img src="images/gep.png" alt="gép" height="150"></td>
+            <td>Centrifuga zajszint</td>
+            <td>68 dB(A)</td>
+        </tr>       
+        <tr>
+            <td>Gőzfunkció</td>
+            <td>Igen</td>
+        </tr>
+        <tr>
+            <td>Töltöttségszenzor</td>
+            <td>Igen</td>
+        </tr>
+        <tr>
+            <td>Súlyautomatika</td>
+            <td>Igen</td>
+        </tr>
+        <tr>
+            <td>Ruhatöltet</td>
+            <td>9 kg</td>
+        </tr>
+        <tr>
+            <td>Automata adagoló technológia</td>
+            <td>TwinDos</td>
+        </tr>
+        <tr>
+            <td>Program időtartama óra:perc (teljes ciklus)</td>
+            <td>1:00</td>
+        </tr>
+        <tr>
+            <td>Maradékidő kijelzés</td>
+            <td>Igen</td>
+        </tr>
+        <tr>
+            <td>Túlfolyás elleni védelem</td>
+            <td>Watercontrol-System</td>
+        </tr>
+       </table>
     </main>
 </body>
 </html>
